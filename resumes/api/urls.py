0372c26463d7b0ac913d866cd862931f510e420b
pemidi <rashidi.peyman@gmail.com>
2023-06-11 03:29:22 +0330

    refactor: sync the endpoints

--- resumes/api/urls.py
+++ resumes/api/urls.py
@@ -4,5 +4,5 @@ from .views import SkillListCreateAPIView, SkillRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('skills', SkillListCreateAPIView.as_view(), name='skills-list-create'),
-    path('skill/<int:pk>', SkillRetrieveUpdateDestroyAPIView.as_view(), name='skill-retrieve-update-destroy')
+    path('skills/<int:pk>', SkillRetrieveUpdateDestroyAPIView.as_view(), name='skill-retrieve-update-destroy')
 ]
