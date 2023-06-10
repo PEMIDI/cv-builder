dfd0aab6c42b808a331249a9d533102ccf8c1678
pemidi <rashidi.peyman@gmail.com>
2023-06-10 03:57:51 +0330

    feat: add custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import ugettext as _
+
+
+class CustomUser(AbstractUser):
+    """
+    CustomUser model extending the AbstractUser model provided by Django's authentication framework.
+
+    Fields:
+    - username: The username of the user (inherited from AbstractUser)
+    - password: The password of the user (inherited from AbstractUser)
+    - email: The email address of the user (inherited from AbstractUser)
+    - first_name: The first name of the user (inherited from AbstractUser)
+    - last_name: The last name of the user (inherited from AbstractUser)
+    - phone_number: The phone number of the user (optional)
+    """
+
+    phone_number = models.CharField(verbose_name=_('phone number'), max_length=12, blank=True)
--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
