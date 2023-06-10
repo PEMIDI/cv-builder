93cc84bc341047cda17da1fc66ab18a74e3ed8a1
pemidi <rashidi.peyman@gmail.com>
2023-06-10 05:46:46 +0330

    feat: register models in admin

--- resumes/admin.py
+++ resumes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Skill, Education, Certificate, Experience, Bio
 
-# Register your models here.
+admin.site.register(Skill)
+admin.site.register(Education)
+admin.site.register(Certificate)
+admin.site.register(Experience)
+admin.site.register(Bio)
