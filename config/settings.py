2443d997b4bd30c8127e5878b3de7280198e27cd
pemidi <rashidi.peyman@gmail.com>
2023-06-10 04:02:04 +0330

    feat: add resume app

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'resumes',
 ]
 
 MIDDLEWARE = [
---
+++ resumes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ResumesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'resumes'
