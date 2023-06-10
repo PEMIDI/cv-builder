cf2159311fa2a2df1cd57ec6e9a03d204a79caaf
pemidi <rashidi.peyman@gmail.com>
2023-06-10 04:25:09 +0330

    fix: add abstract True to the base model

--- utils/models.py
+++ utils/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class BaseModel(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
