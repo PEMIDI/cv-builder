6146b74a56332726babc3bf13115ffd352381971
pemidi <rashidi.peyman@gmail.com>
2023-06-11 07:22:45 +0330

    fix: add comma

--- resumes/api/urls.py
+++ resumes/api/urls.py
@@ -14,7 +14,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', ResumeAPIView.as_view(), name='resume-retrieve')
+    path('', ResumeAPIView.as_view(), name='resume-retrieve'),
 
     path('skills', SkillListCreateAPIView.as_view(), name='skills-list-create'),
     path('skill/<int:pk>', SkillRetrieveUpdateDestroyAPIView.as_view(), name='skill-retrieve-update-destroy'),
