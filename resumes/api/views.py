7d93fbcfc5791c965867cd93119a30d36fd699a3
pemidi <rashidi.peyman@gmail.com>
2023-06-11 07:20:16 +0330

    feat: add permissions

--- resumes/api/views.py
+++ resumes/api/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+from resumes.api.permissions import IsOwner
 from resumes.models import Skill, Education, Certificate, Experience, Bio
 from resumes.api.serializers import (
     SkillSerializer,
@@ -30,7 +31,7 @@ class SkillListCreateAPIView(generics.ListCreateAPIView):
 class SkillRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_update(self, serializer):
         return serializer.save(user=self.request.user)
@@ -51,7 +52,7 @@ class EducationListCreateAPIView(generics.ListCreateAPIView):
 class EducationRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Education.objects.all()
     serializer_class = EducationSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_update(self, serializer):
         return serializer.save(user=self.request.user)
@@ -60,7 +61,7 @@ class EducationRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIVie
 class CertificatesListCreateAPIView(generics.ListCreateAPIView):
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def get_queryset(self):
         return self.queryset.objects.select_related('user').filter(user=self.request.user)
@@ -72,7 +73,7 @@ class CertificatesListCreateAPIView(generics.ListCreateAPIView):
 class CertificateRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_update(self, serializer):
         return serializer.save(user=self.request.user)
@@ -81,7 +82,7 @@ class CertificateRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIV
 class ExperienceListCreateAPIView(generics.ListCreateAPIView):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def get_queryset(self):
         return self.queryset.objects.select_related('user').filter(user=self.request.user)
@@ -93,7 +94,7 @@ class ExperienceListCreateAPIView(generics.ListCreateAPIView):
 class ExperienceRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_update(self, serializer):
         return serializer.save(user=self.request.user)
@@ -103,7 +104,7 @@ class BioCreateRetrieveUpdateDestroyAPIView(generics.CreateAPIView, generics.Ret
                                             generics.UpdateAPIView, generics.DestroyAPIView):
     queryset = Bio.objects.all()
     serializer_class = BioSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def get_object(self):
         return Bio.objects.queryset.objects.select_related('user').filter(user=self.request.user)
