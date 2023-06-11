6d6a0029a93b07a0f0368fc2755b2850eb3c6415
pemidi <rashidi.peyman@gmail.com>
2023-06-11 04:07:31 +0330

    feat: add EducationRetrieveUpdateDestroyAPIView endpoint

--- resumes/api/urls.py
+++ resumes/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import SkillListCreateAPIView, SkillRetrieveUpdateDestroyAPIView, EducationListCreateAPIView
+from .views import SkillListCreateAPIView, SkillRetrieveUpdateDestroyAPIView, EducationListCreateAPIView, \
+    EducationRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('skills', SkillListCreateAPIView.as_view(), name='skills-list-create'),
     path('skills/<int:pk>', SkillRetrieveUpdateDestroyAPIView.as_view(), name='skill-retrieve-update-destroy'),
 
-    path('educations', EducationListCreateAPIView.as_view(), name='educations-list-create')
+    path('educations', EducationListCreateAPIView.as_view(), name='educations-list-create'),
+    path('education/<int:pk>', EducationRetrieveUpdateDestroyAPIView.as_view(),
+         name='education-retrieve-update-destroy')
 ]
