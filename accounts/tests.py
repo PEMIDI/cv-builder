ece4929ac5658d52aebf99b06201d81874c413ce
pemidi <rashidi.peyman@gmail.com>
2023-06-12 06:15:45 +0330

    test: add register login test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserRegistrationTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.register_url = reverse('register')
+
+    def test_register_with_weak_password(self):
+        data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'weak'
+        }
+        response = self.client.post(self.register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_register_with_strong_password(self):
+        data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'StrongPassword123'
+        }
+        response = self.client.post(self.register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser')
+        self.assertEqual(User.objects.get().email, 'testuser@example.com')
+
+    def test_login(self):
+        user = User.objects.create_user(username='testuser', email='testuser@example.com', password='StrongPassword123')
+        url = reverse('login')
+        data = {
+            'username': 'testuser',
+            'password': 'StrongPassword123'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+        self.assertEqual(response.data['user']['username'], 'testuser')
+        self.assertEqual(response.data['user']['email'], 'testuser@example.com')
