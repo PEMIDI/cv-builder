37433061e809826d44cfbc07c34b1c95980d29e1
pemidi <rashidi.peyman@gmail.com>
2023-06-12 06:46:10 +0330

    fix: fix pep8 style

--- config/urls.py
+++ config/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/resume/', include('resumes.api.urls')),
 
-    # Authentication 🔑 
+    # Authentication 🔑
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', RegistrationAPIView.as_view(), name='register'),
