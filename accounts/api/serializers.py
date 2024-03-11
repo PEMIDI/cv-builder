f28272ba65565f303ac7521dc209ab8d0863d3fe
Masiha Ahmadi <esafb52@users.noreply.github.com>
2024-03-11 07:51:37 +0330

    Update serializers.py
    remove duplicate code set user mode

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 
-User = get_user_model()
 
 
 User = get_user_model()
